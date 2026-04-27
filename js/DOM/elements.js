2042b45dd2378737380e9c59e27bfee955fa299f
Ngone060104 <ngonemame67@gmail.com>
2026-04-27 22:29:03 +0000

    feat: ajout de la modale de nouvelle inscriptio style: mise en forme du formulaire d'inscription en deux colonnes , fix: centrage de la modale et activation du scroll interne

--- js/DOM/elements.js
+++ js/DOM/elements.js
@@ -4,6 +4,10 @@ export const domElements = {
     overlay: document.getElementById('overlay'),
     btnMenu: document.querySelector('.btn-menu'),
     btnClose: document.querySelector('.btn-close'),
-    statsContainer: document.querySelector('.grid-cols-1') // le conteneur des cartes
+    statsContainer: document.querySelector('.grid-cols-1'),
+    modal : document.getElementById('modalInscription'),
+    BtnOpen : document.querySelector('#btn-nouvelle-inscription'),
+    BtnClose : document.querySelector('#btn-close-modal')
+     // le conteneur des cartes
 };
 
---
+++ js/app.js
@@ -0,0 +1,5 @@
+import * as DOM from './DOM/elements.js';
+import './pages/dashboard.js';
+import './pages/popup.js';
+import { openModal,closeModal } from './UI/modalRenderer.js';
+import { initNavigation } from './UI/navigationRenderer.js';
